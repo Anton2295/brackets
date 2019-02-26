4abd8bd8b26f9cd44e014112e961daa79084ca78
Anton2295 <MY_NAME@example.com>
2019-02-26 20:57:47 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  var stack = [];
+
+  for(let i = 0;i < str.length;i++)
+  {
+    let headStackValue = stack[stack.length-1];
+    
+    if(isPairOfBreckets(headStackValue,str[i],bracketsConfig))
+      stack.pop();
+    else
+    {
+      if(isOpenBreckets(str[i],bracketsConfig) )
+        stack.push(str[i]);
+      else
+        return false;       
+    }
+    
+  }
+
+
+  if(stack.length == 0)
+    return true;
+  else
+    return false
+}
+
+const openBracketIndex = 0;
+const closeBracketIndex = 1;
+
+function isOpenBreckets( bracket , bracketsConfig){
+  for(let i=0;i< bracketsConfig.length;i++)
+  {
+    if(bracket == bracketsConfig[i][openBracketIndex])
+      return true;
+  }
+  return false;
+}
+
+
+function isPairOfBreckets( openBracket , closeBracket, bracketsConfig){
+  for(let i=0;i< bracketsConfig.length;i++)
+  {
+    if(openBracket == bracketsConfig[i][openBracketIndex])
+      if(closeBracket == bracketsConfig[i][closeBracketIndex])
+        return true;
+      else
+        return false;
+  }
+  return false;
 }
